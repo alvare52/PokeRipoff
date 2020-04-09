cbe1703b015749360d92b11c42a859ada7e6049d
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-04-09 07:09:46 -0700

    added project

---
+++ PokeRipoff/PokeRipoff/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokeRipoff
+//
+//  Created by Jorge Alvarez on 4/9/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokeRipoff/PokeRipoff/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PokeRipoff
+//
+//  Created by Jorge Alvarez on 4/9/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
